0b8e58d4a3e476497f6aa3fc1c0fff703caab3e8
bilal ahmed <bilal.ahhmedd@gmail.com>
2021-07-13 20:42:14 +0500

    Update ebay.py
    handling multiple search string in single crawler, to increase data

--- scraping-ebay-1.0.3/scraping_ebay/spiders/ebay.py
+++ scraping-ebay-1.0.3/scraping_ebay/spiders/ebay.py
@@ -8,23 +8,25 @@ class EbaySpider(scrapy.Spider):
 	name = "ebay"
 	allowed_domains = ["ebay.com"]
 	start_urls = ["https://www.ebay.com"]
-
+	
 	# Allow a custom parameter (-a flag in the scrapy command)
-	def __init__(self, search="Nike",start_page=1,pages=5,size='s'):
-		self.search_string = search
+	def __init__(self, search="Tshirt,laced",pages=4,size='s'):
+		# so first of all split serch based on comma
+		self.search_list = search.split(',')
 		self.pages=int(pages)
 		self.size=size
-		self.start_page=int(start_page)
+		self.prod_urls_tracker = []
 	def parse(self, response):
 		# Extrach the trksid to build a search request	
 		trksid = response.css("input[type='hidden'][name='_trksid']").xpath("@value").extract()[0]       
 		pages=self.pages+1
 		# Build the url and start the requests
-		for x in range(self.start_page,self.pages+1):
-
-			yield scrapy.Request("http://www.ebay.com/sch/i.html?_from=R40&_trksid=" + trksid +
-								"&_nkw=" + self.search_string.replace(' ','+') + "&_ipg=200&_pgn="+str(x), 
-								callback=self.parse_link)
+		for search_string in self.search_list:
+			print('processing string: ',search_string)
+			for x in range(1,self.pages+1):
+				yield scrapy.Request("http://www.ebay.com/sch/i.html?_from=R40&_trksid=" + trksid +
+									"&_nkw=" + search_string.replace(' ','+') + "&_ipg=200&_pgn="+str(x), 
+									callback=self.parse_link)
 
 	# Parse the search results
 	def parse_link(self, response):
@@ -33,62 +35,64 @@ class EbaySpider(scrapy.Spider):
 
 		# Extract info for each product
 		for product in results:		
-			name = product.xpath('.//*[@class="s-item__title"]//text()').extract_first()
-			# Sponsored or New Listing links have a different class
-			if name == None:
-				name = product.xpath('.//*[@class="s-item__title s-item__title--has-tags"]/text()').extract_first()			
-				if name == None:
-					name = product.xpath('.//*[@class="s-item__title s-item__title--has-tags"]//text()').extract_first()			
-			if name == 'New Listing':
-				name = product.xpath('.//*[@class="s-item__title"]//text()').extract()[1]
-
-			# If this get a None result
-			if name == None:
-				name = "ERROR"
-
-			price = product.xpath('.//*[@class="s-item__price"]/text()').extract_first()
-			status = product.xpath('.//*[@class="SECONDARY_INFO"]/text()').extract_first()
-			seller_level = product.xpath('.//*[@class="s-item__etrs-text"]/text()').extract_first()
-			location = product.xpath('.//*[@class="s-item__location s-item__itemLocation"]/text()').extract_first()
+			'''
+			First check if product url is not allready scrapped
+			'''
 			product_url = product.xpath('.//a[@class="s-item__link"]/@href').extract_first()
+			prod_id=product_url.split('itm/')[1].lstrip().split('?')[0]
+			if prod_id not in self.prod_urls_tracker:
+				self.prod_urls_tracker.append(prod_id)
+				name = product.xpath('.//*[@class="s-item__title"]//text()').extract_first()
+				# Sponsored or New Listing links have a different class
+				if name == None:
+					name = product.xpath('.//*[@class="s-item__title s-item__title--has-tags"]/text()').extract_first()			
+					if name == None:
+						name = product.xpath('.//*[@class="s-item__title s-item__title--has-tags"]//text()').extract_first()			
+				if name == 'New Listing':
+					name = product.xpath('.//*[@class="s-item__title"]//text()').extract()[1]
 
-			# Set default values
-			stars = 0
-			ratings = 0
+				# If this get a None result
+				if name == None:
+					name = "ERROR"
 
-			stars_text = product.xpath('.//*[@class="clipped"]/text()').extract_first()
-			if stars_text: stars = stars_text[:3]
-			ratings_text = product.xpath('.//*[@aria-hidden="true"]/text()').extract_first()
-			if ratings_text: ratings = ratings_text.split(' ')[0]
+				price = product.xpath('.//*[@class="s-item__price"]/text()').extract_first()
+				status = product.xpath('.//*[@class="SECONDARY_INFO"]/text()').extract_first()
+				seller_level = product.xpath('.//*[@class="s-item__etrs-text"]/text()').extract_first()
+				location = product.xpath('.//*[@class="s-item__location s-item__itemLocation"]/text()').extract_first()
+				
 
-			summary_data = {
-							"Name":name,
-							"Status":status,
-							"Seller_Level":seller_level,
-							"Location":location,
-							"Price":price,
-							"Stars":stars,
-							"Ratings":ratings,
-							"URL": product_url
-							}
+				# Set default values
+				stars = 0
+				ratings = 0
 
-			# Go to the product details page
-			data = {'summary_data': summary_data}
-			yield scrapy.Request(product_url, meta=data, callback=self.parse_product_details)
+				stars_text = product.xpath('.//*[@class="clipped"]/text()').extract_first()
+				if stars_text: stars = stars_text[:3]
+				ratings_text = product.xpath('.//*[@aria-hidden="true"]/text()').extract_first()
+				if ratings_text: ratings = ratings_text.split(' ')[0]
 
-		# Get the next page
-		'''
-		next_page_url = response.xpath('//*/a[@class="x-pagination__control"][2]/@href').extract_first()
+				summary_data = {
+								"Name":name,
+								"Status":status,
+								"Seller_Level":seller_level,
+								"Location":location,
+								"Price":price,
+								"Stars":stars,
+								"Ratings":ratings,
+								"URL": product_url
+								}
 
-		# The last page do not have a valid url and ends with '#'
-		if next_page_url == None or str(next_page_url).endswith("#"):
-			self.log("eBay products collected successfully !!!")
-		else:
-			print('\n'+'-'*30)
-			print('Next page: {}'.format(next_page_url))
-			yield scrapy.Request(next_page_url, callback=self.parse_link)
-		'''
+				# Go to the product details page
+				data = {'summary_data': summary_data}
+				yield scrapy.Request(product_url, meta=data, callback=self.parse_product_details)
+				
+			else:
+				return 
 
+
+
+
+
+			
 
 	# Parse details page for each product
 	def parse_product_details(self, response):
@@ -163,64 +167,19 @@ class EbaySpider(scrapy.Spider):
 						data[name1]=" ".join(val1)
 					except:
 							pass 
-						
-
-						
-
-
-
-
-
-
-
-					
-
-					 
-				# print("++++++++++++++++++++")
-
-
-
-
-				# name=spect.xpath('.//td/text()')[2].extract()
-				# name=name.strip()
-				# value=spect.xpath('.//td/text()')[3].extract()
-				# val = value.split()
-				
-				# data[name]=" ".join(val)
-
-
-	 
-    
-		# for li in all.xpath('//div[@class="spec-row"]/ul/li'):
-		# 	n=li.xpath('//div[@class="s-name"]/text()').get()
-		# 	v=li.xpath('//div[@class="s-value"]/text()').get()
-		# 	data[n]=v
 		
 
-
+		# append dir_id and images_url to data table		
 		url = data['URL']
 		DirId = url.split('itm/')[1].lstrip().split('?')[0]
 		data['prod_id']=DirId
 		data['images_url']=linklist
 		yield data
-		#yield {'data':data,'images':[linklist,str(DirId)]}
+		
+		
+		
+		
 
-		# yield data
-		# for l in linklist:
-		# 	i=i+1
-			
-		# 	yield{
-        #          'Image Url': l,
-        #          'File number': i,
-        #          'Dir name': str(DirId),
-		# 		 'data':data
-				 
-
-        #         # 'image_urls':linklist
-        #     }
-		
-		
-		
 
 
 
